5064f4c11a64aff73063b753bfb92255e5e2ea89
Grant <grantwoxford@gmail.com>
2026-06-20 15:12:11 -0500

    add movement constants

--- src/main/java/com/gnart/Main.java
+++ src/main/java/com/gnart/Main.java
@@ -3,6 +3,23 @@ package com.gnart;
 import static com.raylib.Colors.*;
 import static com.raylib.Raylib.*;
 
+// Movement constants
+public static final float GRAVITY = 32.0f;
+public static final float MAX_SPEED = 20.0f;
+public static final float CROUCH_SPEED = 5.0f;
+public static final float JUMP_FORCE = 12.0f;
+public static final float MAX_ACCEL = 150.0f;
+// Grounded drag
+public static final float FRICTION = 0.86f;
+// Increasing air drag, increases strafing speed
+public static final float AIR_DRAG = 0.98f;
+// Responsiveness for turning movement direction to looked direction
+public static final float CONTROL = 15.0f;
+public static final float CROUCH_HEIGHT = 0.0f;
+public static final float STAND_HEIGHT = 1.0f;
+public static final float BOTTOM_HEIGHT = 0.5f;
+public static final boolean NORMALIZE_INPUT = true;
+
 public class Main {
     static void main() {
         InitWindow(800, 450, "Demo");
