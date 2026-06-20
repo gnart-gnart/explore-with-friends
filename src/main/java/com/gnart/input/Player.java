ce158d49d3045d92751cbd0a0b16634b0104fd7f
Grant <grantwoxford@gmail.com>
2026-06-20 16:20:26 -0500

    rename playercontroller to java

---
+++ src/main/java/com/gnart/input/Player.java
@@ -0,0 +1,37 @@
+package com.gnart.input;
+
+import com.gnart.entity.Body;
+import com.raylib.Raylib.Vector2;
+
+public class Player {
+    // Movement variables
+    public float maxSpeed = 20.0f;
+    public float crouchSpeed = 5.0f;
+    public float jumpForce = 12.0f;
+    public float maxAccel = 150.0f;
+    // Grounded drag
+    public float friction = 0.86f;
+    // Increasing air drag, increases strafing speed
+    public float airDrag = 0.98f;
+    // Responsiveness for turning movement direction to looked direction
+    public float control = 15.0f;
+    public float crouchHeight = 0.0f;
+    public float standHeight = 1.0f;
+    public float bottomHeight = 0.5f;
+    public boolean normalizeInput = true;
+    // Internal tracking variables
+    private final Vector2 sensitivity = new Vector2().x(0.001f).y(0.001f);
+    private final Vector2 lookRotation = new Vector2().x(0).y(0);
+    private float headTimer = 0.0f;
+    private float walkLerp = 0.0f;
+    private float headLerp = standHeight;
+    private final Vector2 lean = new Vector2().x(0).y(0);
+
+    public Body body;
+
+    public Player(Body body) {
+        this.body = body;
+    }
+
+    public void update()
+}
