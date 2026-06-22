bf5a0c05e4ea67c08d72298369c77aa6a50a9f68
Grant <grantwoxford@gmail.com>
2026-06-22 10:43:44 -0500

    abstract away everything into Game class

--- src/main/java/com/gnart/Main.java
+++ src/main/java/com/gnart/Main.java
@@ -1,41 +1,14 @@
 package com.gnart;
 
-import com.gnart.input.Player;
-import com.raylib.Raylib.Camera3D;
-import static uk.co.electronstudio.jaylib.Jaylib.*;
+import com.gnart.game.Game;
 
 public class Main {
     public static final float GRAVITY = 32.0f;
 
     public static void main(String[] args) {
-        Player player = new Player();
 
-        InitWindow(800, 450, "Explore With Friends");
-        SetTargetFPS(60);
-        Camera3D camera = new Camera3D()
-                ._position(new Vector3(player.position))
-                .target(new Vector3())
-                .up(new Vector3().x(0).y(1).z(0))
-                .fovy(45).projection(CAMERA_PERSPECTIVE);
+        Game game = new Game();
+        game.playGame();
 
-        while (!WindowShouldClose()) {
-            // 1. input
-
-
-            // 2. update
-            player.update();
-            UpdateCamera(camera, CAMERA_ORBITAL);
-
-            // 3. render
-            BeginDrawing();
-            ClearBackground(RAYWHITE);
-            BeginMode3D(camera);
-            DrawGrid(20, 1.0f);
-            EndMode3D();
-            DrawText("Hello world", 190, 200, 20, VIOLET);
-            DrawFPS(20, 20);
-            EndDrawing();
-        }
-        CloseWindow();
     }
 }
